e1b67984646ef47a6b8fe5b9239dfeb5e6ae09de
m7yun <99463715+m7yun@users.noreply.github.com>
2024-01-21 15:00:18 +0800

    Update Coupon.php

--- src/Api/Coupon.php
+++ src/Api/Coupon.php
@@ -20,4 +20,17 @@ class Coupon extends GateWay
 
         return \current($result);
     }
+
+    /**
+     * taobao.tbk.coupon.convert( 淘宝客-推广者-单品券高效转链 )
+     * @link https://open.taobao.com/api.htm?docId=29289&docType=2&scopeId=12486
+     * @param array $params
+     * @return mixed
+     */
+    public function convert(array $params)
+    {
+        $result = $this->send('taobao.tbk.coupon.convert', $params);
+
+        return \current($result);
+    }
 }
